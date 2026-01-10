bcbc44e38a011ed153a67f7f2b05a4ebf1f24ac4
tamzishe <tamzsiwira@gmail.com>
2026-01-09 23:05:20 -0700

    V0.1.3 - Pulled a working image

---
+++ src/api/coverartarchive.js
@@ -0,0 +1,24 @@
+import { CoverArtArchiveApi } from 'musicbrainz-api';
+
+const coverArtArchiveApiClient = new CoverArtArchiveApi();
+
+export async function fetchCoverArt(releaseMbid, coverType = '') {
+    try {
+        const coverInfo = await coverArtArchiveApiClient.getReleaseCovers(releaseMbid);
+        for(const image of coverInfo.images) {
+            if (image.front) {
+                console.log(`Cover art front=${image.front} back=${image.back} url=${image.image}`);
+                return image.image;
+            }
+        }
+        return null;
+    } catch (error) {
+        console.error("Error fetching cover art:", error);
+        return null;
+    }
+}
+
+// fetchCoverArt('976e0677-a480-4a5e-a177-6a86c1900bbf').catch(error => {
+//     console.error(`Failed to fetch cover art: ${error.message}`);
+// })
+export default coverArtArchiveApiClient;
