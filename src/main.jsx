13aadfb381e683b453032162606e6aeea50068aa
tamzishe <tamzsiwira@gmail.com>
2026-06-09 21:47:59 -0600

    bug with routing

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import ListenLaterPage from "./pages/ListenLaterPage.jsx";
 import WantPage from "./pages/WantPage.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import { useAuth } from "./context/AuthContext";
+import FollowPage from "./pages/FollowPage.jsx";
 
 const buildRouter = (session) =>
 	createBrowserRouter([
@@ -56,11 +57,7 @@ const buildRouter = (session) =>
 			element: <WantPage />,
 		},
 		{
-			path: "/user/:username/followers",
-			element: <FollowPage />,
-		},
-		{
-			path: "/user/:username/following",
+			path: "/user/:username/:type",
 			element: <FollowPage />,
 		},
 		{
